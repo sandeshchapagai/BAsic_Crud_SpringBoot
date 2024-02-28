a57bc7bdde215241f40eff3d39a9d10a4aa24c4b
Sandy <sandeshchapagai@users.noreply.github.com>
2024-02-28 13:14:04 +0545

    added dependency for login api called as spring security

---
+++ src/main/java/comnp/sandeshchapagai/example/backend_api/Controller/TeacherController.java
@@ -0,0 +1,12 @@
+package comnp.sandeshchapagai.example.backend_api.Controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/api/v1/student")
+public class TeacherController {
+}
